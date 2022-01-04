318f6dc026e64aa2faffc61061de5b76d6b78abe
muni1430 <93640586+muni1430@users.noreply.github.com>
2022-01-04 14:15:03 +0530

    Create terraformstate.tf

---
+++ terraformstate.tf
@@ -0,0 +1,67 @@
+ {
+  "version": 4,
+  "terraform_version": "1.1.2",
+  "serial": 6,
+  "lineage": "7f66f3c3-a2a8-c49c-d38f-40962a237c9e",
+  "outputs": {},
+  "resources": [
+    {
+      "mode": "managed",
+      "type": "aws_security_group",
+      "name": "allow_tls",
+      "provider": "provider[\"registry.terraform.io/hashicorp/aws\"]",
+      "instances": [
+        {
+          "schema_version": 1,
+          "attributes": {
+            "arn": "arn:aws:ec2:us-east-1:024329274214:security-group/sg-05016b1e921aeb2f5",
+            "description": "Allow TLS inbound traffic",
+            "egress": [
+              {
+                "cidr_blocks": [],
+                "description": "",
+                "from_port": 0,
+                "ipv6_cidr_blocks": [],
+                "prefix_list_ids": [],
+                "protocol": "-1",
+                "security_groups": [],
+                "self": false,
+                "to_port": 0
+              }
+            ],
+            "id": "sg-05016b1e921aeb2f5",
+            "ingress": [
+              {
+                "cidr_blocks": [],
+                "description": "TLS from VPC",
+                "from_port": 443,
+                "ipv6_cidr_blocks": [],
+                "prefix_list_ids": [],
+                "protocol": "tcp",
+                "security_groups": [],
+                "self": false,
+                "to_port": 443
+              }
+            ],
+            "name": "allow_tls",
+            "name_prefix": "",
+            "owner_id": "024329274214",
+            "revoke_rules_on_delete": false,
+            "tags": {
+              "Name": "allow_tls"
+            },
+            "tags_all": {
+              "Name": "allow_tls"
+            },
+            "timeouts": null,
+            "vpc_id": "vpc-0b35f76dd9f315be5"
+          },
+          "sensitive_attributes": [],
+          "private": "eyJlMmJmYjczMC1lY2FhLTExZTYtOGY4OC0zNDM2M2JjN2M0YzAiOnsiY3JlYXRlIjo2MDAwMDAwMDAwMDAsImRlbGV0ZSI6OTAwMDAwMDAwMDAwfSwic2NoZW1hX3ZlcnNpb24iOiIxIn0="
+        }
+      ]
+    }
+  ]
+}
+
+

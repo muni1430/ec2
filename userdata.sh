a613f9b537cbfe7dc51a477f9a81c9cd3b4183ae
muni1430 <93640586+muni1430@users.noreply.github.com>
2022-01-10 19:39:12 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 sudo apt-get update -y
 sudo apt-get upgrade -y
+sudo apt-get install awscli -y
 sudo apt-get install apache2 -y
+sudo chmod -R 777 /var/www/html
+aws s3 cp s3://mydemo-024329274214/dummy.html.txt/var/www/html/index.html
